3cec558841f7198704bb52b5ae02173f3758c1df
willownsenator <willownsenator@gmail.com>
2024-04-27 18:24:40 +0200

    Add Linux Version

---
+++ FirstCplusProject 5.3/Plugins/Developer/RiderLink/Intermediate/Build/Linux/x64/UnrealEditor/Development/RD/RdTextBuffer.cpp.d
@@ -0,0 +1,4 @@
+/home/willownsenator/projects/unreal/FirstCplusProjectUnreal4/FirstCplusProject\ 5.3/Plugins/Developer/RiderLink/Intermediate/Build/Linux/x64/UnrealEditor/Development/RD/RdTextBuffer.cpp.o: \
+  /home/willownsenator/projects/unreal/FirstCplusProjectUnreal4/FirstCplusProject\ 5.3/Plugins/Developer/RiderLink/Source/RD/src/rd_gen_cpp/src/RdTextBuffer.cpp \
+  /home/willownsenator/projects/unreal/FirstCplusProjectUnreal4/FirstCplusProject\ 5.3/Plugins/Developer/RiderLink/Intermediate/Build/Linux/x64/UnrealEditor/Development/RD/Definitions.h \
+  /home/willownsenator/projects/unreal/FirstCplusProjectUnreal4/FirstCplusProject\ 5.3/Plugins/Developer/RiderLink/Source/RD/src/rd_gen_cpp/src/RdTextBuffer.h
---
+++ FirstCplusProject 5.3/Plugins/Developer/RiderLink/Intermediate/Build/Linux/x64/UnrealEditor/Development/RD/hashing.cpp.d
@@ -0,0 +1,3 @@
+/home/willownsenator/projects/unreal/FirstCplusProjectUnreal4/FirstCplusProject\ 5.3/Plugins/Developer/RiderLink/Intermediate/Build/Linux/x64/UnrealEditor/Development/RD/hashing.cpp.o: \
+  /home/willownsenator/projects/unreal/FirstCplusProjectUnreal4/FirstCplusProject\ 5.3/Plugins/Developer/RiderLink/Source/RD/src/rd_framework_cpp/src/main/util/hashing.cpp \
+  /home/willownsenator/projects/unreal/FirstCplusProjectUnreal4/FirstCplusProject\ 5.3/Plugins/Developer/RiderLink/Intermediate/Build/Linux/x64/UnrealEditor/Development/RD/Definitions.h
