6214587bc7e7e01e313f0b039c1886a4b2925d29
willowsenator <willowsenator@gmail.com>
2024-05-26 01:20:22 +0200

    Add different ways of visualize properties in UE by instance and Default

--- FirstCplusProject/Source/FirstCplusProject/GameplayActors/Floater.cpp
+++ FirstCplusProject/Source/FirstCplusProject/GameplayActors/Floater.cpp
@@ -9,13 +9,25 @@ AFloater::AFloater()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CustomStaticMesh"));
+	InitialLocation = FVector(0.0f);
+	PlacedLocation = FVector(0.0f);
+	WorldOrigin = FVector(0.0f, 0.0f, 0.0f);
+	InitialDirection = FVector(0.0f, 0.0f, 0.0f);
+	
+	bInitializeFloaterLocations = false;
+	bShouldFloat = false;
 }
 
 // Called when the game starts or when spawned
 void AFloater::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	PlacedLocation = GetActorLocation();
+
+	if(bInitializeFloaterLocations)
+	{
+		SetActorLocation(InitialLocation);
+	}
 }
 
 // Called every frame
@@ -23,5 +35,10 @@ void AFloater::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if(bShouldFloat)
+	{
+		FHitResult HitResult;
+		AddActorLocalOffset(InitialDirection, false, &HitResult);
+	}
 }
 
--- FirstCplusProject/Source/FirstCplusProject/GameplayActors/Floater.h
+++ FirstCplusProject/Source/FirstCplusProject/GameplayActors/Floater.h
@@ -17,6 +17,25 @@ public:
 	UPROPERTY(VisibleAnywhere, Category="ActorMeshComponents")
 	UStaticMeshComponent *StaticMesh;
 
+	// Location used by SetActorLocation() when BeginPlay() is called
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category="Floater Variables")
+	FVector InitialLocation;
+
+	// Location of the actor when dragged in from the editor
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category="Floater Variables")
+	FVector PlacedLocation;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Floater Variables")
+	FVector WorldOrigin;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Floater Variables")
+	FVector InitialDirection;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Floater Variables")
+	bool bShouldFloat;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Floater Variables")
+	bool bInitializeFloaterLocations;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
