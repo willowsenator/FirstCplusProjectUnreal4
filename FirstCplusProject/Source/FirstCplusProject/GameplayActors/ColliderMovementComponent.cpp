244569d247c1c770ddd8b313dfd996fde7138092
willownsenator <willownsenator@gmail.com>
2024-07-30 01:07:37 +0200

    feat: Add ColliderMovementComponent

---
+++ FirstCplusProject/Source/FirstCplusProject/GameplayActors/ColliderMovementComponent.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ColliderMovementComponent.h"
+
+void UColliderMovementComponent::TickComponent(const float DeltaTime, const enum ELevelTick TickType,
+	FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if(!PawnOwner || !UpdatedComponent || ShouldSkipUpdate(DeltaTime))
+	{
+		return;
+	}
+
+	if (const auto DesiredMovementThisFrame = ConsumeInputVector().GetClampedToMaxSize(1.0f) * DeltaTime * 1500.0f;
+		!DesiredMovementThisFrame.IsNearlyZero())
+	{
+		FHitResult Hit;
+		SafeMoveUpdatedComponent(DesiredMovementThisFrame, UpdatedComponent->GetComponentRotation(), true, Hit);
+
+		// if we bumped into something, try to slide along it
+		if(Hit.IsValidBlockingHit())
+		{
+			SlideAlongSurface(DesiredMovementThisFrame, 1.0f - Hit.Time, Hit.Normal, Hit);
+		}
+	}
+}
+
