61769d84f006dd4b90e89c5536db9622b8f15611
willowsenator <willowsenator@gmail.com>
2024-09-07 15:05:04 +0200

    Add Main Character animation instance and refactor

---
+++ FirstCplusProject/Source/FirstCplusProject/GameplayActors/MainCharacterAnimInstance.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainCharacterAnimInstance.h"
+
+#include "GameFramework/PawnMovementComponent.h"
+
+void UMainCharacterAnimInstance::NativeInitializeAnimation()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+	}
+}
+
+void UMainCharacterAnimInstance::UpdateAnimationProperties()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+	}
+
+	if (Pawn)
+	{
+		const FVector Speed = Pawn->GetVelocity();
+		const FVector LateralSpeed = FVector(Speed.X, Speed.Y, 0.0f);
+		MovementSpeed = LateralSpeed.Size();
+
+		bIsInAir = Pawn->GetMovementComponent()->IsFalling();
+	}
+}
+
