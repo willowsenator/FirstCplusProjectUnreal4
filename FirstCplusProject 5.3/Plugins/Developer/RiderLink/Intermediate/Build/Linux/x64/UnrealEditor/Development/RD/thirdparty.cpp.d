b00385e1348490a38c2f2218f5ab966679bbebb8
willowsenator <willowsenator@gmail.com>
2024-09-07 13:45:01 +0200

    Remove Linux Project

--- FirstCplusProject 5.3/Plugins/Developer/RiderLink/Intermediate/Build/Linux/x64/UnrealEditor/Development/RD/thirdparty.cpp.d
+++
@@ -1,3 +0,0 @@
-/home/willownsenator/projects/unreal/FirstCplusProjectUnreal4/FirstCplusProject\ 5.3/Plugins/Developer/RiderLink/Intermediate/Build/Linux/x64/UnrealEditor/Development/RD/thirdparty.cpp.o: \
-  /home/willownsenator/projects/unreal/FirstCplusProjectUnreal4/FirstCplusProject\ 5.3/Plugins/Developer/RiderLink/Source/RD/thirdparty/thirdparty.cpp \
-  /home/willownsenator/projects/unreal/FirstCplusProjectUnreal4/FirstCplusProject\ 5.3/Plugins/Developer/RiderLink/Intermediate/Build/Linux/x64/UnrealEditor/Development/RD/Definitions.h
