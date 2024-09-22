486d699b529c52ccc60ddace291f7ce5b804db94
willowsenator <willowsenator@gmail.com>
2024-09-23 01:41:58 +0200

    Add floorSwitch and ovelapCollision

---
+++ FirstCplusProject/Source/FirstCplusProject/GameplayActors/FloorSwitch.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloorSwitch.h"
+
+// Sets default values
+AFloorSwitch::AFloorSwitch()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	RootComponent = TriggerBox;
+
+	TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	
+	FloorSwitch = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FloorSwitch"));
+	FloorSwitch->SetupAttachment(GetRootComponent());
+
+	Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
+	Door->SetupAttachment(GetRootComponent());
+
+}
+
+// Called when the game starts or when spawned
+void AFloorSwitch::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapBegin);
+	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &AFloorSwitch::OnOverlapEnd);
+}
+
+// Called every frame
+void AFloorSwitch::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AFloorSwitch::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Start  Overlap with %s"), *OtherActor->GetName());
+}
+
+void AFloorSwitch::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("End Overlap with %s"), *OtherActor->GetName());
+}
+
+
+
+
