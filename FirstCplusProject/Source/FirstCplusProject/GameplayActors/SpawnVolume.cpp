a3fb1b162609fd0d6ee5a5c35bae36162e6dfc0d
willowsenator <willowsenator@gmail.com>
2024-10-16 01:13:11 +0200

    Add Troll to critter_BP and SpawnVolume

---
+++ FirstCplusProject/Source/FirstCplusProject/GameplayActors/SpawnVolume.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpawnVolume.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ASpawnVolume::ASpawnVolume()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpawnBox = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawnBox"));
+}
+
+// Called when the game starts or when spawned
+void ASpawnVolume::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASpawnVolume::Tick(const float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+FVector ASpawnVolume::GetRandomPointInVolume() const
+{
+	const auto SpawnOrigin = SpawnBox->Bounds.Origin;
+	const auto SpawnExtent = SpawnBox->Bounds.BoxExtent;
+
+	return UKismetMathLibrary::RandomPointInBoundingBox(SpawnOrigin, SpawnExtent);
+}
+
