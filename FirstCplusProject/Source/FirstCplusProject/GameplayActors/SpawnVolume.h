771b38e57cccb76ab275f2af3746289772301f82
willowsenator <willowsenator@gmail.com>
2024-10-16 01:13:11 +0200

    Add Troll to critter_BP and SpawnVolume

---
+++ FirstCplusProject/Source/FirstCplusProject/GameplayActors/SpawnVolume.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Critter.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class FIRSTCPLUSPROJECT_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawning")
+	UBoxComponent *SpawnBox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawning")
+	TSubclassOf<ACritter> PawnToSpawn;
+	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintPure, Category = "Spawning")
+	FVector GetRandomPointInVolume() const;
+};
