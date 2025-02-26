19312e8ed00358d29e5de759fa035c18656db19c
willowsenator <willowsenator@gmail.com>
2025-02-26 01:30:13 +0100

    Add MainPlayerController and configure module to use UI for HUD

--- FirstCplusProject/Source/FirstCplusProject/FirstCplusProject.Build.cs
+++ FirstCplusProject/Source/FirstCplusProject/FirstCplusProject.Build.cs
@@ -8,9 +8,9 @@ public class FirstCplusProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Niagara", "UMG" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
