ce679f514d9db82e57fc4a0a64e33826086ac392
willowsenator <willowsenator@gmail.com>
2025-03-01 02:31:59 +0100

    add logic for stamina and Health. Integrate the HUD in the game

--- FirstCplusProject/Source/FirstCplusProject/GameplayActors/MainCharacter.h
+++ FirstCplusProject/Source/FirstCplusProject/GameplayActors/MainCharacter.h
@@ -61,6 +61,23 @@ public:
 	/*** Stop action for jumping */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Enhanced Input")
 	UInputAction* InputStopJump;
+
+	/** PlayerStats */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Player Stats")
+	float MaxHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Player Stats")
+	float Health;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Player Stats")
+	float MaxStamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Player Stats")
+	float Stamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Player Stats")
+	int32 Coins;
+
 	
 protected:
 	// Called when the game starts or when spawned
