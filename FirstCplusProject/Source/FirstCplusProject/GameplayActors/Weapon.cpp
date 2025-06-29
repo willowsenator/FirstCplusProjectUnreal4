9d6e4b56c9ba84d86dedbb3b48926f8e1392fc18
willowsenator <willowsenator@gmail.com>
2025-06-29 23:48:58 +0200

    add socket to right hand character to get weapon

---
+++ FirstCplusProject/Source/FirstCplusProject/GameplayActors/Weapon.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon.h"
+#include "MainCharacter.h"
+#include "Engine/SkeletalMeshSocket.h"
+
+AWeapon::AWeapon()
+{
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("WeaponMesh");
+	StaticMesh->SetupAttachment(GetRootComponent());
+}
+
+
+void AWeapon::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, const int32 OtherBodyIndex, const bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+	if (OtherActor)
+	{
+		if (AMainCharacter* MyCharacter = Cast<AMainCharacter>(OtherActor))
+		{
+			// If the character is valid, equip the weapon
+			Equip(MyCharacter);
+		}
+	}
+	// Additional logic for weapon overlap can be added here
+}
+
+void AWeapon::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, const int32 OtherBodyIndex)
+{
+	Super::OnOverlapEnd(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+	// Additional logic for weapon overlap end can be added here
+}
+
+void AWeapon::Equip(AMainCharacter* Char)
+{
+	if (Char)
+	{
+		StaticMesh->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+		StaticMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
+
+		StaticMesh->SetSimulatePhysics(false);
+
+		if (const USkeletalMeshSocket* RightHandSocket = Char->GetMesh()->GetSocketByName("RightHandSocket"))
+		{
+			RightHandSocket->AttachActor(this, Char->GetMesh());
+			bRotate = false;
+		}
+	}
+}
