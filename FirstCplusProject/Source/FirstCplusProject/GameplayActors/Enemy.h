5c9c0c14ae82af3f742f0a4ee75654548b614540
willowsenator <willowsenator@gmail.com>
2025-10-18 23:43:05 +0200

    enemy blendspace and animInstance

---
+++ FirstCplusProject/Source/FirstCplusProject/GameplayActors/Enemy.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Enemy.generated.h"
+
+UENUM(Blueprintable)
+enum class EEnemyMovementStatus : uint8
+{
+	EMS_Idle UMETA(DisplayName = "Idle"),
+	EMS_MoveToTarget UMETA(DisplayName = "MoveToTarget"),
+	EMS_Attacking UMETA(DisplayName = "Attacking"),
+
+	EMS_MAX UMETA(DisplayName = "DefaultMAX")
+};
+
+UCLASS()
+class FIRSTCPLUSPROJECT_API AEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEnemy();
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Movement")
+	EEnemyMovementStatus EnemyMovementStatus;
+
+	FORCEINLINE void SetEnemyMovementStatus(const EEnemyMovementStatus NewStatus) { EnemyMovementStatus = NewStatus; }
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
