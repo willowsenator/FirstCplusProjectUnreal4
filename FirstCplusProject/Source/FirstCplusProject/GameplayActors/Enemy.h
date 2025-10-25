7416e8e7c76a62ef4982439c7b4dc70278548745
willowsenator <willowsenator@gmail.com>
2025-10-25 02:28:20 +0200

    notify endAttack and moveToTarget if not overlapping CombatSphere

--- FirstCplusProject/Source/FirstCplusProject/GameplayActors/Enemy.h
+++ FirstCplusProject/Source/FirstCplusProject/GameplayActors/Enemy.h
@@ -64,6 +64,12 @@ public:
 	UFUNCTION()
 	virtual void CombatSphereOnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
+	UFUNCTION(BlueprintCallable )
 	void MoveToTarget(const AMainCharacter * Target);
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="AI")
+	bool bOverlappingCombatSphere;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="AI")
+	AMainCharacter *CombatTarget;
 };
